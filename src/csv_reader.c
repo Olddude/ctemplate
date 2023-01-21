7ee2149e4a010a739fda52dfa61edc16b689779d
Konstantin Novikov <nov.konstantin@gmail.com>
2023-01-21 18:18:32 +0000

    fmt

--- src/csv_reader.c
+++ src/csv_reader.c
@@ -4,21 +4,19 @@
 #include "csv_reader.h"
 
 void read_csv_file(const char* file_path, void (*worker_thread)(char*)) {
-        FILE* file = fopen(file_path, "r");
-            if (!file) {
-                        perror("fopen");
-                                return;
-                                    }
-
-                char line[1024];
-                    while (fgets(line, sizeof(line), file)) {
-                                pthread_t thread;
-                                        if (pthread_create(&thread, NULL, worker_thread, line) != 0) {
-                                                        perror("pthread_create");
-                                                                    break;
-                                                                            }
-                                            }
-
-                        fclose(file);
+    FILE* file = fopen(file_path, "r");
+    if (!file) {
+        perror("fopen");
+        return;
+    }
+    char line[1024];
+    while (fgets(line, sizeof(line), file)) {
+        pthread_t thread;
+        if (pthread_create(&thread, NULL, worker_thread, line) != 0) {
+            perror("pthread_create");
+            break;
+        }
+    }
+    fclose(file);
 }
 
