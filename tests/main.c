4e8c19091088f2af0dc3e082263d087771728e03
Konstantin Novikov <konstantin.novikov@knab.nl>
2023-01-21 22:29:12 +0100

    printing

--- tests/main.c
+++ tests/main.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-    printf("size char pointer %lu\n", sizeof(char*));
-    printf("size char %lu\n", sizeof(char));
+    printf("size char pointer %u\n", sizeof(char*));
+    printf("size char %u\n", sizeof(char));
     perror("not implemented");
     return 1;
 }
