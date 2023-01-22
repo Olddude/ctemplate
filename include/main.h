8777e51216a47a474c63b29dc5fcfbd70e26540d
Konstantin Novikov <konstantin.novikov@knab.nl>
2023-01-22 15:43:38 +0100

    cleanup

--- include/main.h
+++ include/main.h
@@ -1,16 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#define BUFFER_SIZE 16384
-
-#include <stdio.h>
-
-int validate(int argc, char* argv[]);
-
-FILE *read_csv(char *file_path);
-
-void print_columns(FILE *csv_file);
-
 int main(int argc, char *argv[]);
 
 #endif
