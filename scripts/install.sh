9896e5a3349262b4c0f5f5634ef7e0e502f79dc3
Konstantin Novikov <konstantin.novikov@knab.nl>
2023-01-25 11:26:05 +0100

    install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,6 +4,18 @@ echo OS: $OSTYPE
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 echo "linux-gnu"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+echo "Mac OSX"
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+echo "POSIX compatibility layer and Linux environment emulation for Windows"
+elif [[ "$OSTYPE" == "msys" ]]; then
+echo "Lightweight shell and GNU utilities compiled for Windows (part of MinGW)"
+elif [[ "$OSTYPE" == "win32" ]]; then
+echo "I'm not sure this can happen."
+elif [[ "$OSTYPE" == "freebsd"* ]]; then
+echo "# ..."
+else
+echo "# Unknown."
 # update
 apk update
 
@@ -25,16 +37,4 @@ apk add \
     libc-dev \
     libcurl \
     libuuid
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-echo "Mac OSX"
-elif [[ "$OSTYPE" == "cygwin" ]]; then
-echo "POSIX compatibility layer and Linux environment emulation for Windows"
-elif [[ "$OSTYPE" == "msys" ]]; then
-echo "Lightweight shell and GNU utilities compiled for Windows (part of MinGW)"
-elif [[ "$OSTYPE" == "win32" ]]; then
-echo "I'm not sure this can happen."
-elif [[ "$OSTYPE" == "freebsd"* ]]; then
-echo "# ..."
-else
-echo "# Unknown."
 fi
